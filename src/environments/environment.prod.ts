53050cd913b2b9a56f6bce682b41bd908656166c
Fábio Fabian Daitx <fabio.daitx@petrobras.com.br>
2021-10-25 09:25:05 -0300

    angular crash online

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://my-json-server.typicode.com/fabiodaitx/angular-crash',
+//  baseUrl: 'https://my-json-server.typicode.com/fabiodaitx/angular-crash',
+  baseUrl: 'https://fabiodaitx-json-server.herokuapp.com',
 };
